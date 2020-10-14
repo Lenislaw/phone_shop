bbac5aa4a0caaac87c0a0c808b1a0ae660a69109
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-10-14 20:19:51 +0200

    Add Product Controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel.js");
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 4;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+module.exports = { getProducts };
