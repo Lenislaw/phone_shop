f3c4f2df29f99b1c641b75d6a11708442ef7574c
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-10-22 21:57:10 +0200

    Add Login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const generateToken = require("../utils/generateToken.js");
 const User = require("../models/userModel.js");
 
 // @desc    Register a new user
@@ -34,6 +35,51 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+
+  console.log(req.user._id)
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 module.exports = {
   registerUser,
+  authUser,
+  getUserProfile,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController.js");
+const { protect, admin } = require("../middleware/authMiddleware.js");
+const {
+  registerUser,
+  authUser,
+  getUserProfile,
+} = require("../controllers/userController.js");
 
+router.route("/profile").get(protect, getUserProfile);
 router.route("/").post(registerUser);
+router.route("/login").post(authUser);
 
 module.exports = router;
