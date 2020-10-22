ad6fbe3412c637d1a93b0f0be7f6025c5a6ddb41
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-10-22 22:02:28 +0200

    Add login,register,profile routes and add switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -11,6 +11,8 @@ import SalesProductsScreen from "./screens/SalesProductsScreen";
 import NotFoundScreen from "./screens/NotFoundScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import UserProfileScreen from "./screens/UserProfileScreen";
+import CartScreen from "./screens/CartScreen";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
@@ -19,36 +21,40 @@ const App = () => {
       <Header />
       <Container maxWidth="lg">
         <main className="App">
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/sales" component={SalesProductsScreen} exact />
-          <Route path="/contact" component={ContactScreen} exact />
-          <Route path="/products" component={ProductsScreen} exact />
-          <Route path="/products/:keyword" component={ProductsScreen} exact />
-          <Route path="/search/:keyword" component={ProductsScreen} exact />
-          <Route
-            path="/search/:keyword/page/:pageNumber"
-            component={ProductsScreen}
-            exact
-          />
-          <Route
-            path="/products/brand/:brand"
-            component={ProductsScreen}
-            exact
-          />
-          <Route
-            path="/products/brand/:brand/page/:pageNumber"
-            component={ProductsScreen}
-            exact
-          />
-          <Route
-            path="/products/page/:pageNumber"
-            component={ProductsScreen}
-            exact
-          />
-          <Route path="/product/:id" component={ProductDetailsScreen} exact />
-          <Route path="/user/Login" component={LoginScreen} exact />
-          <Route path="/user/Register" component={RegisterScreen} exact />
-          <Route path="/notfound" component={NotFoundScreen} exact />
+          <Switch>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/sales" component={SalesProductsScreen} exact />
+            <Route path="/contact" component={ContactScreen} exact />
+            <Route path="/products" component={ProductsScreen} exact />
+            <Route path="/cart" component={CartScreen} exact />
+            <Route path="/products/:keyword" component={ProductsScreen} exact />
+            <Route path="/search/:keyword" component={ProductsScreen} exact />
+            <Route
+              path="/search/:keyword/page/:pageNumber"
+              component={ProductsScreen}
+              exact
+            />
+            <Route
+              path="/products/brand/:brand"
+              component={ProductsScreen}
+              exact
+            />
+            <Route
+              path="/products/brand/:brand/page/:pageNumber"
+              component={ProductsScreen}
+              exact
+            />
+            <Route
+              path="/products/page/:pageNumber"
+              component={ProductsScreen}
+              exact
+            />
+            <Route path="/product/:id" component={ProductDetailsScreen} exact />
+            <Route path="/user/login" component={LoginScreen} exact />
+            <Route path="/user/register" component={RegisterScreen} exact />
+            <Route path="/user/profile" component={UserProfileScreen} exact />
+            <Route path="*" component={NotFoundScreen} />
+          </Switch>
         </main>
       </Container>
       <Footer />
